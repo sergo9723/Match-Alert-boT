4d68261817983b86914edfcd9a5f3a777d258320
sergo9723 <sergo.terzi1997@gmail.com>
2025-06-20 22:16:21 +0300

    Update and rename README.md to main.py

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ ADMIN_ID = os.getenv("ADMIN_ID")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.answer("Бот работает ✅")
-
 
 if name == "main":
     executor.start_polling(dp, skip_updates=True)
